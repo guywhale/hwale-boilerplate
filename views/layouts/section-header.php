b5ad262173c5d8cb2b349c898b3d0d0ea70ba7e6
guywhale <guywhale@gmail.com>
2022-10-14 17:47:17 +0100

    Add hanging red triangle to header

--- views/layouts/section-header.php
+++ views/layouts/section-header.php
@@ -1,8 +1,9 @@
 <header class="bg-grey">
     <div class="w-full h-2 bg-red"></div>
-    <div class="container py-8">
+    <div class="container">
         <div class="flex justify-between">
-            <div class="flex items-center">
+            <div class="relative flex items-center justify-center">
+                <div class="absolute top-0 w-0 h-0 border-transparent border-t-[15px] border-x-[15px] md:border-t-[20px] border-t-red md:border-x-[20px] border-b-0"></div>
                 <?php get_template_part('/views/components/component', 'logo'); ?>
             </div>
             <div>
