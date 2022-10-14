cd1bda78e6679b35c7afbd5cda6966360c02d292
guywhale <guywhale@gmail.com>
2022-10-14 11:09:15 +0100

    Add some styling to ACF Gutenberg blocks in dashboard preview. Previews too narrow, should be full width.

--- app/assets.php
+++ app/assets.php
@@ -12,3 +12,10 @@ function boilerplate_load_assets()
     wp_enqueue_script('ourmainjs', get_theme_file_uri('/build/index.js'), ['wp-element'], '1.0', true);
     wp_enqueue_style('ourmaincss', get_theme_file_uri('/build/index.css'));
 }
+
+add_action('enqueue_block_editor_assets', 'Hwale\App\boilerplate_load_assets_admin');
+function boilerplate_load_assets_admin()
+{
+    wp_enqueue_script('ourmainjs-admin', get_theme_file_uri('/build/index.js'), ['wp-element'], '1.0', true);
+    wp_enqueue_style('ourmaincss-admin', get_theme_file_uri('/build/index.css'));
+}
