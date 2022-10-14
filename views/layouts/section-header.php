a669d47ff86fbc9b3a5045a0c65a51242ced80cf
guywhale <guywhale@gmail.com>
2022-10-14 12:59:22 +0100

    Restore menu functionaltiy to WP

--- views/layouts/section-header.php
+++ views/layouts/section-header.php
@@ -1,7 +1,8 @@
-<header class="bg-gray-300">
-    <div class="container mx-auto">
+<header class="bg-white">  
+    <div class="container">
         <h1 class="py-10 text-3xl text-center">
             <a href="<?php echo get_home_url(); ?>" class="hover:text-blue-500"><?php the_title(); ?></a>
         </h1>
     </div>
+    <?php wp_nav_menu(); ?>
 </header>
