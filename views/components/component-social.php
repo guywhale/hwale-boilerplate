dfc15caf58d18a29c0cfee50ec3d74ff5ddb6995
guywhale <guywhale@gmail.com>
2022-10-16 16:02:23 +0100

    Add some extra y padding to the social media block on mobile

--- views/components/component-social.php
+++ views/components/component-social.php
@@ -3,7 +3,7 @@
 $socials = get_field('sites', 'option') ?: null;
 
 ?>
-<div class="bg-grey-light flex justify-between px-4 py-2 sm:justify-center items-center sm:py-[28px] lg:py-[18px] sm:px-8 h-full">
+<div class="bg-grey-light flex justify-between p-4 sm:justify-center items-center sm:py-[28px] lg:py-[18px] sm:px-8 h-full">
     <?php if ($socials) {
         foreach ($socials as $social) { ?>
             <a class="block mx-2 sm:mx-4"
