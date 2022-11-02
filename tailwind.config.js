47010a8b51d2e8f576d54a60ee656d1895c57152
Guy Whale <guywhale@gmail.com>
2022-11-02 14:22:39 +0000

    Add TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.php", "./**/*.php", "./src/**/*.js", "!**/vendor/**"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
