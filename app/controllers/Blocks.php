bd43352fb19493f46ffaeaff9ee5d25f85fdfdc4
Guy Whale <guywhale@gmail.com>
2022-11-03 02:14:12 +0000

    Fix Blocks controller path

--- app/controllers/Blocks.php
+++ app/controllers/Blocks.php
@@ -13,4 +13,16 @@ abstract class Blocks extends Controller
     {
         self::$viewType = 'block';
     }
+
+    protected static function render(string $viewType, string $viewFile, array $data = [])
+    {
+        $path = self::$viewsPath;
+        parent::render($viewType, $viewFile, $data);
+
+        if (!is_file(get_template_directory() . "{$path}{$viewType}s/{$viewFile}/{$viewFile}.php")) {
+            throw new \Exception("Sorry, {$viewFile}.php doesn't exist in {$path}{$viewType}s/.");
+        }
+
+        include get_template_directory() . "{$path}{$viewType}s/{$viewFile}/{$viewFile}.php";
+    }
 }
