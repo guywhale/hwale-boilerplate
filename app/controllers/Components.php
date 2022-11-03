968ee2504b3ac3cee10b6e72839bc20f734d55df
Guy Whale <guywhale@gmail.com>
2022-11-03 02:16:39 +0000

    Update path to files in Components controller

--- app/controllers/Components.php
+++ app/controllers/Components.php
@@ -13,4 +13,16 @@ abstract class Components extends Controller
     {
         self::$viewType = 'component';
     }
+
+    protected static function render(string $viewType, string $viewFile, array $data = [])
+    {
+        $path = self::$viewsPath;
+        parent::render($viewType, $viewFile, $data);
+
+        if (!is_file(get_template_directory() . "{$path}{$viewType}s/{$viewFile}.php")) {
+            throw new \Exception("Sorry, {$viewFile}.php doesn't exist in {$path}{$viewType}s/.");
+        }
+
+        include get_template_directory() . "{$path}{$viewType}s/{$viewFile}.php";
+    }
 }
