49b9d739028bd586382e3984756ab51bd5d616d7
Guy Whale <guywhale@gmail.com>
2022-11-03 16:06:42 +0000

    Style footer

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -6,7 +6,7 @@
 
 ?>
 
-<footer>
+<footer class="py-4 text-white bg-gray-600">
     <div class="container">
         <?php if ($copyright) { ?>
             <p class="text-center"><?= $copyright; ?></p>
