d0c7197e52f5bd90756737de4222f72faf2c33dc
Guy Whale <guywhale@gmail.com>
2022-11-05 16:44:47 +0000

    Set up vanilla app.js file with Alpine.js installed

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
-
-
+import Alpine from 'alpinejs';
 
 document.addEventListener('DOMContentLoaded', () => {
+  /**
+   * Initialize Alpine
+   */
+  window.Alpine = Alpine;
+  Alpine.start();
 
+  /**
+   * Main init function
+   */
+  init();
+
+  function init() {
+    console.log('Hello from app.js');
+  }
 });
