d78c0fdbe9d5924f092eae2d8a3cbe4e49411818
Guy Whale <guywhale@gmail.com>
2022-11-05 14:04:48 +0000

    Add hwale.svg to illustrate how the getSVG() function works

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -1,13 +1,18 @@
 <?php
 
+use function Hwale\getSVG;
+
 [
     'title' => $title
 ] = $data;
 
 ?>
 
-<header class='layout-header'>
+<header class='py-16 text-center layout-header'>
+    <a href="/" class="inline-block w-20 h-auto transition-colors fill-red-800 hover:fill-gray-600">
+        <?php getSVG('hwale.svg'); ?>
+    </a>
     <?php if ($title) { ?>
-        <h1 class="py-16 text-center"><?= $title; ?></h1>
+        <h1 class="text-center"><?= $title; ?></h1>
     <?php } ?>
 </header>
