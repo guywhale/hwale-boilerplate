7f3691391389ac32a52c858485b439b6816c2e73
Guy Whale <guywhale@gmail.com>
2022-11-05 16:35:31 +0000

    Remove all React as won't appear in Gutenberg previews

--- src/app.js
+++ src/app.js
@@ -1,13 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import ExampleReactComponent from './js/components/ExampleReactComponent';
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
-  /**
-   * Example React Component
-   */
-  if (document.querySelector('[data-example-react-component]')) {
-    const exampleReactComponents = document.querySelectorAll('[data-example-react-component]');
-    [...exampleReactComponents].map(el => ReactDOM.render(<ExampleReactComponent />, el));
-  }
+
 });
