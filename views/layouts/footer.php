4df4149701de4f8b816ef7b8695bfa7518cd3ae2
Guy Whale <guywhale@gmail.com>
2022-11-05 14:11:14 +0000

    Make copyright message link back to my site

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -7,9 +7,9 @@
 ?>
 
 <footer class="py-4 text-white bg-gray-600 layout-footer">
-    <div class="container">
+    <div class="container text-center">
         <?php if ($copyright) { ?>
-            <p class="text-center"><?= $copyright; ?></p>
+            <a href="https://guywhale.com" target="_blank" rel="noopener"><?= $copyright; ?></p>
         <?php } ?>
     </div>
 </footer>
