1bf223b4f84f0c439a7abcc1c02836eb6843c651
Guy Whale <guywhale@gmail.com>
2022-11-22 17:54:07 +0000

    Fix footer closing tag issue.

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -9,7 +9,7 @@
 <footer class="py-4 text-white bg-gray-600 layout-footer">
     <div class="container text-center">
         <?php if ($copyright) { ?>
-            <a href="https://guywhale.com" target="_blank" rel="noopener"><?= $copyright; ?></p>
+            <a href="https://guywhale.com" target="_blank" rel="noopener"><?= $copyright; ?></a>
         <?php } ?>
     </div>
 </footer>
